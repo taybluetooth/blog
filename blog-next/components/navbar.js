d1f4b8b0dba604f39360866a8b8c956290dcee20
Callum Taylor <callumtaylorbusiness@gmail.com>
2021-06-08 22:21:26 +0100

    Made more changes to post slug page.

---
+++ blog-next/components/navbar.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+
+    const [color, setColor] = useState(false);
+
+    const changeNavbarColor = () => {
+        if(window.scrollY >= 400) {
+            setColor(true)
+        }
+        else {
+            setColor(false)
+        }
+    }
+    React.useEffect(() => {
+        window.addEventListener('scroll', changeNavbarColor);
+    })
+
+    return (
+        <div className={color ? 'navbar colorChange' : 'navbar'}>
+            <div className='navbar-content'>
+                <div className='navbar-content-left'>
+                    <img className='icon' src='computer-icon.png'></img>
+                    <Link href='/'>Callum Taylor</Link>
+                </div>
+                <div className='navbar-content-right'>
+                    <Link href='/'>Home</Link>
+                    <a>About</a>
+                    <a>Projects</a>
+                    <a>Contact</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
--- blog-next/pages/[slug].js
+++ blog-next/pages/[slug].js
@@ -1,24 +1,17 @@
-import Link from 'next/link'
-import Markdown from 'markdown-to-jsx';
+import Markdown from 'markdown-to-jsx'
+import Navbar from '../components/navbar'
 
 export default function Post({ post }) {
-    
+    /* make each component seperate */
     return (
         <div className='slug-page'>
-            <div className="navbar">
-                <div className="navbar-content">
-                    <a>Home</a>
-                    <a>About</a>
-                    <a>Projects</a>
-                    <a>Contact</a>
-                </div>
-            </div>
+            <Navbar />
             <div className='slug-header-section'>
                 <div className='image-square'>
                     {/*change url when in production!*/} 
                     <img src={`http://localhost:1337` + post.image.url}></img>
                 </div>
-                <h2>{post.title}</h2>
+                <h1>{post.title}</h1>
                 <h2>{post.user.username} - {post.user.created_at.substring(0,10)}</h2>
             </div>
             <div className='slug-post-section'>
@@ -30,8 +23,9 @@ export default function Post({ post }) {
                 </div>
             </div>
             <div className='slug-footer-section'>
-                <p>SOCIAL MEDIA LINKS</p>
-                <p>CONTACT DETAILS</p>
+                <p>Developed by Callum Taylor (2021)</p>
+                <p>Powered by React and Next.js</p>
+                <p>Content managed by Strapi</p>
             </div>
         </div>
     )
